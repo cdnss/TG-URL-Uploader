2fa8bad6e43f1a6103417b93223392c7c9f361d4
cdnss <32522595+cdnss@users.noreply.github.com>
2023-12-01 19:55:49 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,9 +11,9 @@ logger = logging.getLogger(__name__)
 import os
 
 # the secret configuration specific things
-if bool(os.environ.get("WEBHOOK", False)):
+#if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
-else:
+#else:
    # from config import Config
 
 import pyrogram
